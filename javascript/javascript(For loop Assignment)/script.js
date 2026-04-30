f3247db057afbe1e6c7d0fff0600bf79cd2c8c60
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-04-30 19:29:41 +0530

    Add HTML and JavaScript files for loop and conditional statement assignments

---
+++ javascript/javascript(For loop Assignment)/script.js
@@ -0,0 +1,16 @@
+for(var i=1;i<=5;i++)
+{
+    console.log(i);
+}
+for(var i=1;i<=10;i++)
+{
+    console.log(i);
+}
+for(var i=10;i>0;i--)
+{
+    console.log(i);
+}
+for(var i=2;i<10;i++)
+{
+    console.log(i);
+}
---
+++ jsConditionalstatement/script.js
@@ -0,0 +1,49 @@
+let n = 95;
+
+if(n>=90){
+    console.log("A");
+}
+else if(n>=75){
+    console.log("B");
+}
+else if(n>=50){
+    console.log("C");
+}
+else{
+    console.log("fail");
+}
+
+
+let usr="abcde"
+let pass="12345"
+if(usr=== "abcde" && pass==="12345"){
+    console.log("login success");
+}
+else{
+ console.log("login failed")
+}
+
+let n1=0;
+if(n1%2==0){
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}
+
+let a=1;
+let b=5;
+console.log(a>2 || b>6)
+
+let data;
+let name="";
+data=name || "N/A"
+console.log(data);
+//if(name){
+ //   data=name;
+//} else{
+  //  data="N/A";
+//}
+//console.log(data);
+
+
