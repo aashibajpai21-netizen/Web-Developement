99a73628bd624b0862cc3d6921694cb9bc8bbba5
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-03 17:20:26 +0530

    Add additional for loop examples to script.js for enhanced practice

--- javascript for loop Assignment/script.js
+++ javascript for loop Assignment/script.js
@@ -5,3 +5,24 @@ for(var i=0;i<=5;i++)
 for(var i=0;i<=10;i++){
     console.log(i);
 }
+for(var i=10;i>0;i--){
+    console.log(i);
+}
+for(var i=2;i<=10;i+=2){
+    console.log(i);
+}
+for(var i=1;i<=9;i+=2){
+    console.log(i);
+}
+for(var i=1;i<=10;i++){
+    console.log("15 x "+ i + "="+(15*i));
+}
+for(var i=1;i<=20;i+=2){
+    console.log(i);
+}
+for(var i=5;i<=50;i+=5){
+    console.log(i);
+}
+for(var i=1;i<=10;i++){
+    console.log("i*i" + "="+ (i*i))
+}
