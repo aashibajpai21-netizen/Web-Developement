b77dd0bb0b5405aeac5345172795df7e60be9ddd
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-03 18:04:30 +0530

    Add initial HTML and JavaScript files for while loop assignment

---
+++ jswhileloopAssignment/script.js
@@ -0,0 +1,34 @@
+ let i=1;
+ while(i<=5){
+    console.log(i);
+    i++;
+ }
+ let i1=5;
+ while( i1>=1){
+    console.log(i1);
+    i1--;
+ }
+ let i2=2;
+ while(i2<11){
+    console.log(i2);
+    i2+=2;
+ }
+ let i3=10;
+let sum=0;
+while(i3>=5){
+sum=sum+i3;
+i3--;
+}
+console.log("total sum ="+ sum);
+     
+ let i4=0;
+ while(i4<=10){
+    console.log(i4);
+    i4++;
+ }
+ let i5=0;
+ while(i5<=5){
+    console.log(i5);
+    i5++;
+
+ }
