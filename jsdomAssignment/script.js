c3e3a5b887def341c95c3f55397b2268858d996d
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-05 14:41:14 +0530

    Add HTML, CSS, and JavaScript files for interactive content and DOM manipulation

---
+++ jsdomAssignment/script.js
@@ -0,0 +1,23 @@
+  document.getElementById("heading").innerHTML="Heading changed";
+
+
+    function changeText() {
+            document.getElementById("text").innerHTML ="Paragraph text changed!";
+    
+        }
+        function submit(){
+            document.getElementById("textheading").innerHTML="Welcome";
+        }
+      document.getElementById("para");
+      console.log(para.innerHTML);
+      document.getElementById("title").innerHTML = "Content Changed Using DOM";
+      function changetext1(){
+         document.getElementById("text1").innerHTML="Changed!";
+      }
+document.getElementById("myworld").innerHTML="my papa";
+document.getElementById("myheart").innerHTML="my mom";
+//the whole html page is known as document
+//html tags are called element nodes <head><body><p><h1>
+//<h1></h1> and <p> are text nodes
+ const fn=document.getElementById("pg").innerText ;
+console.log(fn);
