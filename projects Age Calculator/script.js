6de04d9a80217fb51d7297632d22f24ac7d21e72
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-10 00:25:17 +0530

    Fix input type for DOB and add age display functionality in Age Calculator

--- projects Age Calculator/script.js
+++ projects Age Calculator/script.js
@@ -4,9 +4,21 @@ document.querySelector("form").addEventListener("submit",(event) => {
     const curDate = document.getElementById("CurrentDate").value ;
 
 
-    const Age=NUmber(curDate.split("-")[0])-Number(dob.split("-")[0]);
-    documenjt.getElementById("DOB").value=" ";
+    const Age=Number(curDate.split("-")[0])-Number(dob.split("-")[0]);
+    
+    const birthMonth = Number(dob.split("-")[1]);
+     const currentMonth = Number(curDate.split("-")[1]);
+      const birthDay = Number(dob.split("-")[2]);
+       const currentDay = Number(curDate.split("-")[2]);
+       if(
+        currentMonth < birthMonth ||
+        (currentMonth === birthMonth && currentDay < birthDay)
+    ){
+        Age--;
+    }
+     document.getElementById("DOB").value=" ";
     document.getElementById("CurrentDate").value="";
     document.getElementById("MyAge").innerText = Age;
+
     
 });
