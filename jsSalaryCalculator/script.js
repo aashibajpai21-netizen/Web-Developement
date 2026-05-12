ec7612b9f2ce77a7d974ebf1056be629a25d2799
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-12 18:48:57 +0530

    Remove unused variables from Salary Calculator script

--- jsSalaryCalculator/script.js
+++ jsSalaryCalculator/script.js
@@ -5,15 +5,6 @@ const resetBtn = document.getElementById("resetBtn");
 const errorMessage = document.getElementById("errorMessage");
 const successMessage = document.getElementById("successMessage");
 
-const spinner = document.getElementById("spinner");
-const btnText = document.getElementById("btnText");
-
-const resultCard = document.getElementById("resultCard");
-
-const resultBasic = document.getElementById("resultBasic");
-const resultHra = document.getElementById("resultHra");
-const resultDa = document.getElementById("resultDa");
-const resultGross = document.getElementById("resultGross");
 
 /*
   Salary Rules:
