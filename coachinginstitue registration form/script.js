f88accd1f773a1e7cc72f711fab36e8023e898a1
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-14 20:13:20 +0530

    Refactor registration form script: rename variables for clarity, add email validation, and improve data handling

--- coachinginstitue registration form/script.js
+++ coachinginstitue registration form/script.js
@@ -4,7 +4,36 @@ document
     event.preventDefault();
 
     const fn = document.getElementById("fullName").value;
-    const gndr = document.querySelector("input[name='gender']:checked")?.value;
+    const gender = document.querySelector("input[name='gender']:checked")?.value;
+     const email = document.getElementById("email").value;
+    
+     const phone = document.getElementById("phone").value;
+      const dob = document.getElementById("dob").value;
+       const qualification = document.getElementById("qualification").value;
+        const marks = document.getElementById("marks").value;
+         const course = document.getElementById("course").value;
+          const markss= document.getElementById("marks").value;
+          
+           const city = document.getElementById("city").value;
+            const pincode = document.getElementById("pin").value;
+         
+
+         const registrationData = {
+          "fullName":  fn,
+          "Email ": email,
+          "gender": gender,
+          "phone": phone,
+          "dob": dob,
+          "qualification": qualification,
+          "marks": marks,
+          "course": course,
+          "city": city,
+          "pincode": pin,
+
+
+         }
+
+  
 
     const timings = [];
 
@@ -19,7 +48,7 @@ document
     console.log(timings);
   });
 
-// ResetButton
+// ResetButton //date calculation 
 document
   .getElementById("registrationFrom")
   .addEventListener("reset", (event) => {
@@ -27,3 +56,16 @@ document
 
     window.location.reload();
   });
+
+  function validateData(data){
+    let isValid = true;
+    if(!data.fullName){
+      document.getElementById
+    }
+  }
+  function validateData(data{
+    let isValid = true;
+    if(!/^[A-Za-z\d\.\_]+ @(gmail.com|outlook.com$/.test(data.Email)){
+      document.getElementById("emailError").innerText="Only standard email format allowed with gmail.com"
+    }
+  })
