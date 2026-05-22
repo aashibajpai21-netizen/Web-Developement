86bae2beb6cdaa71092b2583163e2addea9fe9b5
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-05-22 23:34:20 +0530

    Refactor script.js: reorganize functions and add console logs for better execution flow

--- executioncontext/script.js
+++ executioncontext/script.js
@@ -12,3 +12,40 @@ function test() {
 }
 
 test();
+function functionA() {
+    console.log("Inside Function A");
+
+    functionB();
+
+    console.log("Back to Function A");
+}
+
+function functionB() {
+    console.log("Inside Function B");
+}
+
+console.log("Start");
+
+functionA();
+
+console.log("End");
+
+// Global variable
+let message = "Hello from Global Scope";
+
+function testScope() {
+
+    let message = "Hello from Function Scope";
+
+    console.log("Inside Function:", message);
+}
+testScope();
+
+// Accessing global variable
+console.log("Outside Function:", message);
+// Non-Strict Mode
+function nonStrictFunction() {
+    console.log("Non-Strict Mode:", this);
+}
+
+nonStrictFunction();
