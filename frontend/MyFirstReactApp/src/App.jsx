04fe3f670ca41d534455f128ac79a02221954080
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-01 20:34:09 +0530

    routing added in all pages

--- frontend/MyFirstReactApp/src/App.jsx
+++ frontend/MyFirstReactApp/src/App.jsx
@@ -1,21 +1,33 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import ContactUs from "./Pages/ContactUs";
+import Login from "./Pages/Login";
+import Product from "./Pages/Product";
+import Register from "./Pages/Register";
+
+
+import {BrowserRouter,R } from "react-router-dom";
 
 import React from "react";
 
 //the function name is same as the file or folder name like in java class has the same name as the file
+//page reload is not possible in react
+
+
 function App() {
   return (
     <>
-      <Header />
-      <div id="abcd">My first React App</div>
-      <p className="bg-primary ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quis
-        laborum eos ea sint autem et quidem blanditiis, officiis temporibus,
-        fugiat dolore, culpa quam dolores odio! Nulla, neque. Sunt, voluptates!
-      </p>
 
-      <Footer />
+   
+      <Home/>
+      <About/>
+      <ContactUs/>
+      <Login/>
+      <Product/>
+      <Register/>
+      
     </>
   );
 }
--- frontend/MyFirstReactApp/src/Components/Header.jsx
+++ frontend/MyFirstReactApp/src/Components/Header.jsx
@@ -1,8 +1,29 @@
-function Header(){
-    return (
+function Header() {
+  return (
     <>
-    <div id="cart1">This is Header</div>
+      <div className="bg-primary-subtle p-2 d-flex justify-content-between align-item-center">
+        <span className="text-primary fs-4 fw-bold">My Company</span>
+        <div className="d-flex gap-4">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/Product"}>Product</Link>
+          <Link to={"/ContactUs"}>ContactUs</Link>
+
+          <span>Home</span>
+          <span>About</span>
+          <span>Contact Us</span>
+          <span>Product</span>
+        </div>
+        <div className="d-flex gap-3">
+          <Link to={"/Login"}>
+            <button className="btn btn-outline-primary">Login</button>
+          </Link>
+          <Link to={"/Register"}>
+            <button className="btn btn-outline-primary">Register</button>
+          </Link>
+        </div>
+      </div>
     </>
-    );
+  );
 }
 export default Header;
