0afe00e5b40432f11071996913cbde812fe82b21
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-06 20:39:16 +0530

    feat: enhance ContactUs and Login components with form handling and improved styling

--- frontend/MyFirstReactApp/src/Pages/ContactUs.jsx
+++ frontend/MyFirstReactApp/src/Pages/ContactUs.jsx
@@ -1,8 +1,44 @@
+import {useState} from "react";
+
 function ContactUs() {
+  const [userName, setUserName] = useState("");
+  const [phoneNumber , setPhoneNumber] = useState("");
+const [email , setEmail] = useState("");
+
+
+
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">Contact Us</span>
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Contact Us!!!</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="userName">Username</label>
+            <input
+              type="text"
+              name="userName"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input
+              type="text"
+              name="phoneNumber"
+              className="form-control"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <br />
+            <div className="float-end">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
