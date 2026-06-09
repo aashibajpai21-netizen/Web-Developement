a296e5b5f2855c01fd853f50266563bde94e4f5c
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-09 23:58:16 +0530

    asyncawaitassignment

---
+++ asyncawaitassignment/script.js
@@ -0,0 +1,7 @@
+async function greet() {
+    return "Hello Students";
+}
+
+greet().then((result) => {
+    console.log(result);
+});
--- frontend/cravingsReactApp/src/App.jsx
+++ frontend/cravingsReactApp/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     
       <div className=" p-1 bg-orange-500 flex items-center justify-between ">
         <div className="w-20 mx-3 ">
-          <img src={logo} alt="bg image " />
+          <img src={logo} alt="bg image"/>
         </div>
         <div>
           <button className=" bg-orange-500  hover: outline  rounded text-light fs-6 px-4 p-2 ">
@@ -16,6 +16,8 @@ const App = () => {
           <button className=" bg-amber-50 hover:bg-orange-500 rounded text-danger fs-6 px-4 p-2 ">
             Register
           </button>
+
+          ghghghghhghghghghghghghghghggh
         </div>
       </div>
     </>
