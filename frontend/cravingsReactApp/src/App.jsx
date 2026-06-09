2c63b8453c7736d9d7155a8381e5187b503d00bb
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-09 20:31:29 +0530

    styling in cravings

--- frontend/cravingsReactApp/src/App.jsx
+++ frontend/cravingsReactApp/src/App.jsx
@@ -1,19 +1,24 @@
-
 import logo from "./assets/cravings.png";
 
-
-
-const App = () =>  {
+const App = () => {
   return (
     <>
-
-    <div className="   p-3 bg-orange-500"></div>
-    <button className=" bg-amber-50 hover:bg-orange-500 rounded text-light fs-5 px-4 p-3"> Login</button>
-    <img src={logo} alt="bg image" />;
+      <div className=" p-1 bg-orange-500 flex items-center justify-between ">
+        <div className="w-20 mx-3 ">
+          <img src={logo} alt="bg image " />
+        </div>
+        <div>
+          <button className=" bg-orange-500  hover: outline  rounded text-light fs-6 px-4 p-2 ">
+            {" "}
+            Login
+          </button>
+          <button className=" bg-amber-50 hover:bg-orange-500 rounded text-danger fs-6 px-4 p-2 ">
+            Register
+          </button>
+        </div>
+      </div>
     </>
-                        
   );
-    
-}
+};
 
 export default App;
