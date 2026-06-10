e6eae16f5800444b34241493a8a3f03c79b0ea7d
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-11 00:00:33 +0530

    feat: Add step execution and task completion functions with async/await

--- asyncawaitassignment/script.js
+++ asyncawaitassignment/script.js
@@ -20,3 +20,44 @@ async function fetchData() {
 }
 
 fetchData();
+
+
+function step1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Step 1 completed");
+      resolve();
+    }, 1000);
+  });
+}
+
+function step2() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Step 2 completed");
+      resolve();
+    }, 1000);
+  });
+}
+
+async function executeSteps() {
+  await step1(); // Wait for step1 to finish
+  await step2(); // Then execute step2
+}
+
+executeSteps();
+
+function task() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000); // Wait for 2 seconds
+  });
+}
+
+async function completeTask() {
+  await task();
+  console.log("Task Completed");
+}
+
+completeTask();
