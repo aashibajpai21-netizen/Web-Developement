a0380f4b9f725b0de785e6c14611be6fd0940a5a
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-12 19:32:05 +0530

    feat: Refactor project structure with routing, header, and page components; remove unused CSS

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,35 +1,27 @@
 import React from "react"
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { BrowserRouter,Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
 import ContactUs from "./pages/ContactUs";
 import Register from "./pages/Register";
-import { BrowserRouter,Routes, Route } from "react-router-dom"; 
+
 const App = () => {
-  return(
+  return (
     <>
-      <div className="bg-primary">App</div>
       <BrowserRouter>
-      <Header/>
-      <Footer/>
-      <Routes>
-        <route>
-          <Link to={"  / "}>Home</Link>
-          <Link to={" /  "}>ContactUs</Link>
-          <Link to={" / "}>Product</Link>
-          <Link to={" / "}>Register</Link>
-          <Link to={" / "}>About</Link>
-                 
-        </route>
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </BrowserRouter>
-       
     </>
-
   );
-}
+};
+
 export default App;
-
-
--- Project/src/pages/About.jsx
+++ Project/src/pages/About.jsx
@@ -6,4 +6,4 @@ function About() {
   );
 }
 
-export default Footer;
+export default About;
